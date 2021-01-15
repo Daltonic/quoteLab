b06d83e09bceb3a194b8225947af7bebfbe08d33
Daltonic <darlingtongospel@gmail.com>
2021-01-15 12:18:00 +0100

    Feat: applies two way binding of text inputs to quote image.

--- src/components/Lab.vue
+++ src/components/Lab.vue
@@ -1,24 +1,39 @@
 <template>
   <div class="lab__container">
     <div class="lab__card">
-      <p>
-        In a world mostly wiped out by the plague and embroiled in an elemental
-        struggle between good and evil, the fate of mankind rests on the frail
-        should...
-      </p>
-      <small>Nicolas Tesla</small>
+      <p>{{ quote }}</p>
+      <small>{{ name }}</small>
     </div>
     <div class="lab__tools">
-        <h4>Eith Quote</h4>
-        <input class="lab__form" type="text" placeholder="Your name">
-        <textarea class="lab__form" placeholder="Remember, be nice!" cols="30" rows="10" maxlength="50"></textarea>
-        <button class="lab__btn">Capture</button>
+      <h4>Eith Quote</h4>
+      <textarea
+        class="lab__form"
+        placeholder="Remember, be nice!"
+        cols="30"
+        rows="10"
+        v-model="quote"
+        maxlength="150"
+      ></textarea>
+      <input
+        class="lab__form"
+        type="text"
+        placeholder="Your name"
+        v-model="name"
+      />
+      <button class="lab__btn">Capture</button>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      name: "",
+      quote: "",
+    };
+  },
+};
 </script>
 
 <style scoped>
@@ -42,74 +57,75 @@ export default {};
   border-radius: 9px;
   padding: 20px;
   object-fit: contain;
-  transition: transform .45s;
+  transition: transform 0.45s;
   color: white;
-  box-shadow: 0 8px 20px 0 rgba(0,0,0,.15);
+  box-shadow: 0 8px 20px 0 rgba(0, 0, 0, 0.15);
 }
 
 .lab__card > p {
-    font-size: 30px;
-    font-style: italic;
-    margin-bottom: 10px;
+  font-size: 30px;
+  font-style: italic;
+  margin-bottom: 10px;
 }
 
 .lab__card > small {
-    font-style: bold;
-    float: right;
+  font-style: bold;
+  float: right;
 }
 
 .lab__tools {
-    display: flex;
+  display: flex;
   flex-wrap: wrap;
   justify-content: space-around;
-    width: 50%;
-    background-color: #c0decd;
-    border-radius: 9px;
-    padding: 20px 50px;
+  width: 50%;
+  background-color: #c0decd;
+  border-radius: 9px;
+  padding: 20px 50px;
 }
 
 .lab__tools > * {
-    margin: 10px 0;
-    width: 100%;
+  margin: 10px 0;
+  width: 100%;
 }
 
 .lab__tools > textarea {
-    width: 100%;
+  width: 100%;
 }
 
 .lab__form {
-    border: none;
-    border-radius: 9px;
-    padding: 10px;
-    box-shadow: 0 8px 20px 0 rgba(0,0,0,.15);
+  border: none;
+  border-radius: 9px;
+  padding: 10px;
+  box-shadow: 0 8px 20px 0 rgba(0, 0, 0, 0.15);
 }
 
 .lab__btn {
-    border: none;
-    cursor: pointer;
-    padding: 10px;
-    background: white;
-    border-radius: 9px;
-    box-shadow: 0 8px 20px 0 rgba(0,0,0,.15);
-    transition: all .2s ease-in-out;
+  border: none;
+  cursor: pointer;
+  padding: 10px;
+  background: white;
+  border-radius: 9px;
+  box-shadow: 0 8px 20px 0 rgba(0, 0, 0, 0.15);
+  transition: all 0.2s ease-in-out;
 }
 
 .lab__btn:hover {
-    background: transparent;
-    color: white;
+  background: transparent;
+  color: white;
 }
 
-@media screen and (max-width: 992px){
-    .lab__tools, .lab__card {
-        width: 100%;
-    }
+@media screen and (max-width: 992px) {
+  .lab__tools,
+  .lab__card {
+    width: 100%;
+  }
 
-    .lab__tools {
-        margin: 40px 0;
-    }
+  .lab__tools {
+    margin: 40px 0;
+  }
 
-    .lab__card > p {
-        padding: 5rem 0;
-    }
+  .lab__card > p {
+    padding: 5rem 0;
+  }
 }
 </style>
--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="result__container">
-    <div class="result__card" v-for="n in images" :key="n" :class="{result_selected: n == selected}">
-      <img class="result__image" src="@/assets/images/search.png" alt="Cobra Kai" @click="onSelected(n)">
+    <div
+      class="result__card"
+      v-for="n in images"
+      :key="n"
+      :class="{ result_selected: n == selected }"
+    >
+      <img
+        class="result__image"
+        src="@/assets/images/search.png"
+        alt="Cobra Kai"
+        @click="onSelected(n)"
+      />
     </div>
     <div class="break"></div>
     <button class="result__btn">Load More</button>
@@ -10,29 +20,30 @@
 
 <script>
 export default {
-  name: 'app-result',
+  name: "app-result",
   data() {
     return {
       images: [1, 2, 3, 4, 5, 6],
-      selected: null
-    }
+      selected: null,
+    };
   },
   methods: {
     onSelected(item) {
-      this.selected = item
-      console.log(item)
-    }
-  }
-}
+      this.selected = item;
+      console.log(item);
+    },
+    
+  },
+};
 </script>
 
 <style scoped>
 .result__container {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-around;
-    max-width: 80%;
-    margin: 50px auto;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  max-width: 80%;
+  margin: 50px auto;
 }
 
 .result__card {
@@ -45,39 +56,39 @@ export default {
   object-fit: contain;
   width: 100%;
   max-height: 250px;
-  transition: all .2s ease-in-out;
-  box-shadow: 0 8px 20px 0 rgba(0,0,0,.15);
+  transition: all 0.2s ease-in-out;
+  box-shadow: 0 8px 20px 0 rgba(0, 0, 0, 0.15);
   cursor: pointer;
   overflow: hidden;
 }
 
 .result__image:hover {
-    transform: scale(1.11);
-    box-shadow: 0 8px 20px 0 rgba(0,173,95, .30);
-    border-radius: 9px;
+  transform: scale(1.11);
+  box-shadow: 0 8px 20px 0 rgba(0, 173, 95, 0.3);
+  border-radius: 9px;
 }
 
 .result_selected {
   transform: scale(1.11);
-    box-shadow: 0 8px 20px 0 #FFC107;
-    border-radius: 9px;
+  box-shadow: 0 8px 20px 0 #ffc107;
+  border-radius: 9px;
 }
 
 .result__btn {
-    border: transparent;
-    cursor: pointer;
-    padding: 10px;
-    background: #00ad5f;
-    color: white;
-    border-radius: 9px;
-    box-shadow: 0 8px 20px 0 rgba(0,0,0,.15);
-    transition: all .2s ease-in-out;
-    margin-top: 40px;
+  border: transparent;
+  cursor: pointer;
+  padding: 10px;
+  background: #00ad5f;
+  color: white;
+  border-radius: 9px;
+  box-shadow: 0 8px 20px 0 rgba(0, 0, 0, 0.15);
+  transition: all 0.2s ease-in-out;
+  margin-top: 40px;
 }
 
 .result__btn:hover {
-    background: transparent;
-    color: #00ad5f;
+  background: transparent;
+  color: #00ad5f;
 }
 
 .break {
@@ -85,13 +96,13 @@ export default {
   height: 0;
 }
 
-@media ( max-width: 992px) {
+@media (max-width: 992px) {
   .result__card {
     width: 40%;
   }
 }
 
-@media ( max-width: 768px) {
+@media (max-width: 768px) {
   .result__card {
     width: 90%;
   }
